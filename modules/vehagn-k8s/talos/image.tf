8597ca787efe5bcff626b5c3ee7faec378ab3bcb
Sebastian Klamar <git@sebastian.klamar.name>
2024-11-17 08:46:48 +0100

    fix(vehagn-k8s): remove dynamic part of schematic for proxmox_virtual_environment_download_file
    this commit implements solution option #4 for circumventing vehagn/homelab#106

--- modules/vehagn-k8s/talos/image.tf
+++ modules/vehagn-k8s/talos/image.tf
@@ -6,58 +6,32 @@ locals {
   update_version   = coalesce(var.image.update_version, var.image.version)
   update_schematic = coalesce(var.image.update_schematic, local.schematic)
   update_image_id  = "${talos_image_factory_schematic.updated.id}_${local.update_version}"
-
-  skl_workaround_orig_key    = "orig-image"
-  skl_workaround_updated_key = "updated-image"
 }
 
 resource "talos_image_factory_schematic" "this" {
   schematic = local.schematic
 }
 
+# not used (see https://github.com/vehagn/homelab/issues/106)
+# TODO: support change of schematic upon update
 resource "talos_image_factory_schematic" "updated" {
   schematic = local.update_schematic
 }
 
-resource "proxmox_virtual_environment_download_file" "orig-image" {
-  for_each = toset(distinct([for i in var.nodes : "${i.host_node}_no-schematic-id_${local.version}"]))
+resource "proxmox_virtual_environment_download_file" "this" {
+  # this.id format: <node>_<schematic>_<version>
+  # for_each = toset(distinct([for k, v in var.nodes : "${v.host_node}_${v.update == true ? local.update_image_id : local.image_id}"]))
+  # this.id format: <node>_<version>
+  for_each = toset(distinct([for k, v in var.nodes : "${v.host_node}_${v.update == true ? local.update_version : local.version}"]))
 
   node_name    = split("_", each.key)[0]
   content_type = "iso"
   datastore_id = var.image.proxmox_datastore
 
-  file_name               = "talos-${talos_image_factory_schematic.this.id}-${split("_", each.key)[2]}-${var.image.platform}-${var.image.arch}.img"
-  url                     = "${var.image.factory_url}/image/${talos_image_factory_schematic.this.id}/${split("_", each.key)[2]}/${var.image.platform}-${var.image.arch}.raw.gz"
+  # ex.: talos-ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515-v1.8.1-nocloud-amd64.img
+  file_name               = "talos-${talos_image_factory_schematic.this.id}-${split("_", each.key)[1]}-${var.image.platform}-${var.image.arch}.img"
+  url                     = "${var.image.factory_url}/image/${talos_image_factory_schematic.this.id}/${split("_", each.key)[1]}/${var.image.platform}-${var.image.arch}.raw.gz"
   decompression_algorithm = "gz"
   overwrite               = false
+  overwrite_unmanaged     = true
 }
-
-resource "proxmox_virtual_environment_download_file" "updated-image" {
-  for_each = toset(distinct([for i in var.nodes : "${i.host_node}_no-schematic-id_${local.version}"]))
-
-  node_name    = split("_", each.key)[0]
-  content_type = "iso"
-  datastore_id = var.image.proxmox_datastore
-
-  file_name               = "talos-${talos_image_factory_schematic.updated.id}-${split("_", each.key)[2]}-${var.image.platform}-${var.image.arch}.img"
-  url                     = "${var.image.factory_url}/image/${talos_image_factory_schematic.updated.id}/${split("_", each.key)[2]}/${var.image.platform}-${var.image.arch}.raw.gz"
-  decompression_algorithm = "gz"
-  overwrite               = false
-}
-
-# resource "proxmox_virtual_environment_download_file" "this" {
-#   # for_each = toset(distinct([for k, v in var.nodes : "${v.host_node}_${v.update == true ? local.update_image_id : local.image_id}"]))
-#   for_each = toset(distinct([for k, v in var.nodes : "${v.host_node}_${v.update == true ? local.skl_workaround_orig_key : local.skl_workaround_updated_key}"]))
-#   # for_each = toset([ "pve2_${local.image_id}", "pve2_${local.update_image_id}" ])
-#   # for_each = toset([ "pve2_orig_1.2.3", "pve2_update_3.2.1}" ])
-
-#   node_name    = split("_", each.key)[0]
-#   content_type = "iso"
-#   datastore_id = var.image.proxmox_datastore
-
-#   # ex.: talos-ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515-v1.8.1-nocloud-amd64.img
-#   file_name               = "talos-${split("_", each.key)[1]}-${split("_", each.key)[2]}-${var.image.platform}-${var.image.arch}.img"
-#   url                     = "${var.image.factory_url}/image/${split("_", each.key)[1]}/${split("_", each.key)[2]}/${var.image.platform}-${var.image.arch}.raw.gz"
-#   decompression_algorithm = "gz"
-#   overwrite               = false
-# }
